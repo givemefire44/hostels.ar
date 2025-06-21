793308c37624ba8449a812a004e6a6c39f9d4d50
givemefire44 <damefuego2020@gmail.com>
2025-06-21 16:20:20 -0300

    Update Container.jsx

--- app/components/Container.jsx
+++ app/components/Container.jsx
@@ -3,7 +3,7 @@ export default function Container({ children, style = {} }) {
   return (
     <div
       style={{
-        maxWidth: 1400,
+        maxWidth: 1350,
         width: "100%",
         margin: "0 auto",
         padding: "0 24px",
