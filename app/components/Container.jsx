0ec63a0b0243d62db774ef89805b4efe378d4807
givemefire44 <damefuego2020@gmail.com>
2025-06-20 23:47:50 -0300

    Create Container.jsx

---
+++ app/components/Container.jsx
@@ -0,0 +1,16 @@
+"use client";
+export default function Container({ children, style = {} }) {
+  return (
+    <div
+      style={{
+        maxWidth: 1200,
+        width: "100%",
+        margin: "0 auto",
+        padding: "0 24px",
+        ...style,
+      }}
+    >
+      {children}
+    </div>
+  );
+}
