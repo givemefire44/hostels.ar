69f21162f3ddf01e6d78555c584dea6d7089ee23
givemefire44 <damefuego2020@gmail.com>
2025-06-21 01:23:48 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Hero from "./components/HeroHostel";
+import HeroHostel from "./components/HeroHostel";
 import TestimonialCarousel from "./components/TestimonialCarousel";
 import HostelGrid from "./components/HostelMosaic";
 
 export default function HomePage() {
   return (
     <>
-      <Hero />
+      <HeroHostel />
       <TestimonialCarousel />
       <HostelGrid />
     </>
