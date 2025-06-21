d6a7c82a98b14ecbf1118bc89af0cc9f3197886c
givemefire44 <damefuego2020@gmail.com>
2025-06-21 14:48:10 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Container from "./components/Container";
-import Header from "./components/Header";
 import MainHeroLayout from "./layouts/MainHeroLayout";
 import TestimonialCarousel from "./components/TestimonialCarousel";
 import HostelGrid from "./components/HostelMosaic";
@@ -8,9 +7,7 @@ import Footer from "./components/Footer";
 export default function HomePage() {
   return (
     <>
-      {/* Header fijo arriba */}
-      <Header />
-      {/* Hero bajado 80px */}
+     
       <Container>
         <MainHeroLayout />
       </Container>
