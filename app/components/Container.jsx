8c057e52fb24b32231ef032910eb876a85bd2a36
givemefire44 <damefuego2020@gmail.com>
2025-06-22 18:01:59 -0300

    Update Container.jsx

--- app/components/Container.jsx
+++ app/components/Container.jsx
@@ -1,15 +1,7 @@
 "use client";
-export default function Container({ children, style = {} }) {
+export default function Container({ children, className = "" }) {
   return (
-    <div
-      style={{
-        maxWidth: 1350,
-        width: "100%",
-        margin: "0 auto",
-        padding: "0 24px",
-        ...style,
-      }}
-    >
+    <div className={`container ${className}`}>
       {children}
     </div>
   );
