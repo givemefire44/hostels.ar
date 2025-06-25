08dd8b23e31b447639cfb11f02ff81f95b04e559
givemefire44 <damefuego2020@gmail.com>
2025-06-24 21:06:11 -0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Reemplaza estos valores por los de tu proyecto Supabase
-const supabaseUrl = 'TU_SUPABASE_URL';
+const supabaseUrl = 'https://pyyioonvcpbuoxghakgs.supabase.co';
 const supabaseAnonKey = 'TU_SUPABASE_ANON_KEY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
