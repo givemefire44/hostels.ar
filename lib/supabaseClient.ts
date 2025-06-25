ec808c201ed8737b4ea4c442f8b457b1e0402e66
givemefire44 <damefuego2020@gmail.com>
2025-06-24 21:07:24 -0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Reemplaza estos valores por los de tu proyecto Supabase
 const supabaseUrl = 'https://pyyioonvcpbuoxghakgs.supabase.co';
-const supabaseAnonKey = 'TU_SUPABASE_ANON_KEY';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5eWlvb252Y3BidW94Z2hha2dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MDgxODUsImV4cCI6MjA2NjM4NDE4NX0.p-meiWJ28zD9ZcWOoxd2CUzoDR6BBaNnmV9gFkazuiU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
