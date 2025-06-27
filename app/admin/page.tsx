b88fcd11397951545d5001be4f5667dd36bd4efb
givemefire44 <damefuego2020@gmail.com>
2025-06-26 22:54:32 -0300

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -462,6 +462,7 @@ export default function NuevaEntradaPage() {
           <SeoSnippet />
           <h3 style={{margin:"32px 0 12px", color: "#333" }}>Vista previa del contenido:</h3>
           <div
+            className="preview-content"
             style={{
               border: "1px solid #ccc",
               borderRadius: 4,
